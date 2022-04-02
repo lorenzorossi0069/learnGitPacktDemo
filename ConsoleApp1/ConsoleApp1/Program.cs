d5523778baa7b8aae5109b351e8e47db5accb2d9
Lorenzo Rossi <lorenzor@tiscali.it>
2022-04-02 11:43:54 +0200

    update sab1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("in visual studio");
+            Console.WriteLine("in visual studio ");
         }
     }
 }
